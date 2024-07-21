ad00f77da3172155ff25f60971868f6713ebf58d
ChousX <gmanenergy@gmail.com>
2024-07-21 00:04:27 -0400

    new file

--- src/soul/mod.rs
+++ src/soul/mod.rs
@@ -1,9 +1,13 @@
 use crate::prelude::*;
+
 mod personality;
 pub use personality::*;
 
 mod gender;
 pub use gender::*;
+
+mod failings;
+pub use failings::*;
 
 pub struct SoulPlugin;
 impl Plugin for SoulPlugin{

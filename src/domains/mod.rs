b48d81fab170568c80a31acc76d68affa96f53e7
ChousX <gmanenergy@gmail.com>
2024-07-28 09:41:12 -0400

    made the map bigger

--- src/domains/mod.rs
+++ src/domains/mod.rs
@@ -40,7 +40,7 @@ pub fn add_domain(
     mut commands: Commands,
     assets: Res<TerrainTextures>
 ) {
-    let map_size = TilemapSize { x: 70, y: 70 };
+    let map_size = TilemapSize { x: 255, y: 255 };
     let mut tile_storage = TileStorage::empty(map_size);
     let tilemap_entity = commands.spawn_empty().id();
     let tilemap_id = TilemapId(tilemap_entity);
